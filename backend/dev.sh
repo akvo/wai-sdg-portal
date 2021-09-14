2e5f3a3e0785f103820df32292376e48264154af
dedenbangkit <mail@dedenbangkit.com>
2021-09-15 00:11:51 +0700

    [#1] Frontend setup

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 set -eu
+
+apk update && apk add postgresql-dev gcc python3-dev musl-dev
+
 pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt
 pip check
