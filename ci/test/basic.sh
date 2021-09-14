ce899e91706e8aaeb6cbbc990d4ca2d301f3b981
dedenbangkit <mail@dedenbangkit.com>
2021-09-15 02:35:13 +0700

    [#1] Wait for port 8000

--- ci/test/basic.sh
+++ ci/test/basic.sh
@@ -3,7 +3,7 @@
 
 set -euo pipefail
 
-wait4ports -q -s 1 -t 60 tcp://localhost:80 tcp://localhost:5000
+wait4ports -q -s 1 -t 60 tcp://localhost:80 tcp://localhost:5000 tcp://localhost:8000
 
 http_get() {
     url="${1}"
