dfae8b9ccb10a0c0d9232075998ad3e5407ef598
dedenbangkit <mail@dedenbangkit.com>
2021-09-15 02:18:33 +0700

    [#1] Change workflow 🔥 🔥 🔥

---
+++ backend/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eu
+
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
