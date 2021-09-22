a36093704faad9625676cb40d968d9689e400258
dedenbangkit <mail@dedenbangkit.com>
2021-09-22 18:39:51 +0700

    [#3] Administration data generator
    - Source: ethiopia.topo.json
    - Command: `cd ./doc && ./administration-generator.sh`

---
+++ doc/administration-generator.sh
@@ -0,0 +1,15 @@
+jq -r '[.objects
+    | .arsi_negele_shashemene
+    | .[]?
+    | [.[]?.properties
+    | {
+        "UNIT_ID": .W_CODE,
+        "RK_CODE": .RK_CODE,
+        "UNIT_TYPE": .UNIT_TYPE,
+        "UNIT_NAME": .UNIT_NAME,
+       }]
+   | .[]?]
+   | (.[0] | keys_unsorted) as $keys
+    | $keys, map([.[ $keys[] ]])[]
+   | @csv' ./ethiopia.topo.json > ../backend/data/administration-ethiopia.csv
+
