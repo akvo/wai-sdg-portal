afb30e00c5cd99409dfa92ecea24adc56a2cd7d8
wayangalihpratama <wgprtm@gmail.com>
2021-10-11 20:09:22 +0800

    [#12] Add page skeleton

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+const Home = () => {
+  return (
+    <Row>
+      <Col lg={12}>Home page</Col>
+      <Col lg={12}>Col 2</Col>
+    </Row>
+  );
+};
+
+export default Home;
