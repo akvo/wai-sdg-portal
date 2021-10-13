9eacff334116cc8e18e110edb1254933da3a7eda
dedenbangkit <mail@dedenbangkit.com>
2021-10-13 19:32:43 +0700

    [#21] JS Color Variable Example

---
+++ frontend/src/util/color.js
@@ -0,0 +1,14 @@
+export const JMPColor = {
+  sanitation: {
+    basic: "#ddd",
+    advanced: "#ddd",
+    someothername: "#ddd",
+  },
+  health: {
+    somethingelse: "#ddd",
+  },
+};
+
+export const OtherNameColor = {
+  test: "#ccc",
+};
