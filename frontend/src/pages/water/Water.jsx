95fff626f811b4b5694274a96a97aa656e7e9852
dedenbangkit <mail@dedenbangkit.com>
2021-10-18 22:50:16 +0700

    [#24] Add dataTable component at water page

--- frontend/src/pages/water/Water.jsx
+++ frontend/src/pages/water/Water.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Row,
   Col,
@@ -9,8 +9,10 @@ import {
   Input,
   Carousel,
   Card,
+  Table,
 } from "antd";
 import { Link } from "react-router-dom";
+import api from "../../util/api";
 
 import "./water.scss";
 import Map from "../../components/Map";
@@ -19,7 +21,45 @@ import { SelectFilter } from "../../components/common";
 
 const { Search } = Input;
 
+const columns = [
+  { title: "Water Points", dataIndex: "name", key: "name" },
+  { title: "Number of Users", dataIndex: 82, key: 82 },
+  { title: "Water Source Type", dataIndex: 79, key: 79 },
+  { title: "Energy Source", dataIndex: 81, key: 81 },
+];
+
 const Water = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (loading) {
+      api
+        .get("/data/form/5", null, { param: { page: 1, perpage: 10 } })
+        .then((d) => {
+          const tableData = d.data.data.map((x) => {
+            return {
+              key: x.id,
+              name: x.name,
+              82: x.answer.find((a) => a.question === 82)?.value,
+              79: x.answer.find((a) => a.question === 79)?.value,
+              81: x.answer.find((a) => a.question === 81)?.value,
+              detail: x.answer,
+            };
+          });
+          setData(tableData);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+        });
+    }
+  }, []);
+
+  if (loading) {
+    return "";
+  }
+
   return (
     <Row className="water-container">
       {/* Filter */}
@@ -59,6 +99,22 @@ const Water = () => {
                 </Col>
               </Row>
               <Divider />
+              <Row>
+                <Col span={24}>
+                  <Table
+                    size="small"
+                    columns={columns}
+                    expandable={{
+                      expandedRowRender: (record) => (
+                        <p style={{ margin: 0 }}>{record.description}</p>
+                      ),
+                      rowExpandable: (record) =>
+                        record.name !== "Not Expandable",
+                    }}
+                    dataSource={data}
+                  />
+                </Col>
+              </Row>
               <Row align="middle" justify="space-between" wrap={true}>
                 <Link to="/form/water-point-data-upload/5">
                   <Button>Add New</Button>
