031d649b977c6da939ef35f2e95eba75c4d0fc36
wayangalihpratama <wgprtm@gmail.com>
2021-10-18 11:54:55 +0800

    [#16] Improve isAuthCookie check function

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -4,5 +4,6 @@ export const isAuthCookie = () => {
   return document.cookie
     .split(";")
     .map((item) => trim(item))
-    .includes("_legacy_auth0.is.authenticated=true");
+    .find((x) => x.includes("_legacy_auth0."))
+    ?.includes(".is.authenticated=true");
 };
