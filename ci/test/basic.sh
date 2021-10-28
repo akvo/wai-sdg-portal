f40b3fe9ceefa172e74f038f6c7378185cad57da
dedenbangkit <mail@dedenbangkit.com>
2021-10-29 06:01:10 +0700

    [#53] Add retries to psql

--- ci/test/basic.sh
+++ ci/test/basic.sh
@@ -3,10 +3,11 @@
 
 set -euo pipefail
 
-#waiting for postgres
-until psql --host db:5432 --username wai -w &>/dev/null
+RETRIES=10
+
+until psql -h db:5432 -U wai -w password -d wai_ethiopia -c "select 1" &>/dev/null 2>&1 || [ $RETRIES -eq 0 ];
 do
-  echo "Waiting for PostgreSQL..."
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
   sleep 1
 done
 
