0aa961abd05b3611533f47133aac0fe4f682c2cd
dedenbangkit <mail@dedenbangkit.com>
2021-10-29 06:27:33 +0700

    [#3] DB Seeder shortcut

--- backend/seed.sh
+++ backend/seed.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-python -m seeder.user 30
+if [[ $# -eq 2 ]] ; then
+    echo "$1 $2"
+fi
+
+if [[ $# -eq 3 ]] ; then
+    python -m seeder.administration
+    python -m seeder.admin "$@"
+    python -m seeder.user 30 "$3"
+    python -m seeder.form
+    python -m seeder.datapoint "$1"
+else
+    echo ""
+    echo "This script require more than $# argument/s"
+    echo ""
+    echo "Example: ./test.sh dev@akvo.org \"My Name\" \"My Organisation\""
+    echo ""
+fi
