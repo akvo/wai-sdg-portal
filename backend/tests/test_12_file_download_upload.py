9b9758af0748cfe3b222d0d00810edccc23b4ec2
dedenbangkit <mail@dedenbangkit.com>
2021-11-01 05:47:35 +0700

    [#50] Download excel template test

---
+++ backend/tests/test_12_file_download_upload.py
@@ -0,0 +1,27 @@
+import sys
+import pytest
+from fastapi import FastAPI
+from httpx import AsyncClient
+from tests.test_01_auth import Acc
+from sqlalchemy.orm import Session
+
+pytestmark = pytest.mark.asyncio
+sys.path.append("..")
+
+account = Acc(True)
+ftype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+
+
+class TestFileRoutes():
+    @pytest.mark.asyncio
+    async def test_download_excel_template(self, app: FastAPI,
+                                           session: Session,
+                                           client: AsyncClient) -> None:
+        res = await client.get(
+            app.url_path_for("excel-template:get_by_form_id", id=1),
+            headers={"Authorization": f"Bearer {account.token}"})
+        assert res.status_code == 200
+        headers = res.headers
+        assert headers["content-type"] == ftype
+        assert headers[
+            "content-disposition"] == 'attachment; filename="1-test.xlsx"'
