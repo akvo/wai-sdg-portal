7f3e3ffbd711fd688e767cdc3bc327099b08b1dd
dedenbangkit <mail@dedenbangkit.com>
2021-11-02 02:55:35 +0700

    [#55] Upload excel template tests

--- backend/tests/test_12_file_download_upload.py
+++ backend/tests/test_12_file_download_upload.py
@@ -1,5 +1,8 @@
 import sys
+import aiofiles
+import os
 import pytest
+import pandas as pd
 from fastapi import FastAPI
 from httpx import AsyncClient
 from tests.test_01_auth import Acc
@@ -25,3 +28,37 @@ class TestFileRoutes():
         assert headers["content-type"] == ftype
         assert headers[
             "content-disposition"] == 'attachment; filename="1-test.xlsx"'
+
+    @pytest.mark.asyncio
+    async def test_upload_excel_template(self, app: FastAPI, session: Session,
+                                         client: AsyncClient) -> None:
+        excel_file = "./tmp/1-test.xlsx"
+        correct_data = [[
+            "Option 4", "23,23", "Testing Data 1", 20, "Option A", "2020-12-18"
+        ], [
+            "Option 2", "24,24", "Testing Data 2", 23, "Option B", "2020-12-18"
+        ]]
+        columns = [
+            "2|Test Option Question", "3|Test Geo Question",
+            "4|Test Datapoint Text Question", "5|Test Number Question",
+            "6|Test Multiple Option Question", "7|Test Date Question"
+        ]
+        df = pd.DataFrame(correct_data, columns=columns)
+        df.to_excel(excel_file, index=False)
+        fname = excel_file.split("/")[-1]
+        async with aiofiles.open(excel_file, 'rb') as of:
+            contents = await of.read()
+        res = await client.post(
+            app.url_path_for("excel-template:post",
+                             form_id=1,
+                             administration=4),
+            files={"file": (fname, contents, ftype)},
+            headers={"Authorization": f"Bearer {account.token}"})
+        assert res.status_code == 200
+        res = res.json()
+        assert res["attempt"] == 0
+        assert res["available"] is None
+        assert res["status"] == "pending"
+        assert res["type"] == "validate_data"
+        assert res["info"] == {"form_id": 1, "administration": 4}
+        os.remove(excel_file)
