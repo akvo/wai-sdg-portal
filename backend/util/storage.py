00f63da4e4d28e1908dce838baffaad8df542158
dedenbangkit <mail@dedenbangkit.com>
2021-11-02 02:54:32 +0700

    [#49] Fake cloud storage service for running test

--- backend/util/storage.py
+++ backend/util/storage.py
@@ -1,12 +1,19 @@
 import os
+from pathlib import Path
 from google.cloud import storage
+import shutil
 
 bucket_name = "wai-ethiopia"
+TESTING = os.environ.get("TESTING")
 
 
 def upload(file: str, folder: str, filename: str = None):
     if not filename:
         filename = file.split("/")[-1]
+    if TESTING:
+        fake_location = f"./tmp/fake-storage/{filename}"
+        shutil.copy2(fake_location, file)
+        return fake_location
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     destination_blob_name = f"{folder}/{filename}"
@@ -20,6 +27,9 @@ def upload(file: str, folder: str, filename: str = None):
 def delete(url: str):
     file = url.split("/")[-1]
     folder = url.split("/")[-2]
+    if TESTING:
+        os.remove(f"./tmp/{url}")
+        return url
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(f"{folder}/{file}")
@@ -28,6 +38,9 @@ def delete(url: str):
 
 
 def check(url: str):
+    if TESTING:
+        path = Path(url)
+        return path.is_file()
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     return storage.Blob(bucket=bucket, name=url).exists(storage_client)
