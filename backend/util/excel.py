ca0a7ee5ba850d3922a2330950f62deeea91c7b8
dedenbangkit <mail@dedenbangkit.com>
2021-11-02 23:58:09 +0700

    [#58] Add administration level to template

--- backend/util/excel.py
+++ backend/util/excel.py
@@ -3,24 +3,48 @@ import pandas as pd
 import enum
 import humps
 import itertools
-from db import crud_question
+from db import crud_question, crud_form, crud_administration
 from datetime import datetime
-from db import crud_form
 from models.question import Question, QuestionType
 from sqlalchemy.orm import Session
 from string import ascii_uppercase
 
 
+def generate_administration_sheet(session: Session):
+    adm = crud_administration.get_administration(session=session)
+    df = pd.DataFrame([a.serialize for a in adm])
+    df["parent"] = df["parent"].fillna(0).astype(int)
+    dict_id = df.set_index('id').parent.to_dict()
+
+    def get_parent_id(anc):
+        anc = [anc] if not isinstance(anc, list) else anc
+        if anc[-1] == 0:
+            return anc
+        else:
+            parent = get_parent_id([dict_id[anc[-1]]])
+            anc += parent
+            return anc
+
+    dict_name = df.set_index('id').name.to_dict()
+    df['path_id'] = df.id.apply(get_parent_id)
+    df['parent'] = df.apply(
+        lambda x:
+        [dict_name[id_] for id_ in x.path_id if not (id_ == x.id or id_ == 0)],
+        axis=1)
+    df['parent'] = df['parent'].apply(lambda x: " | ".join(x))
+    return df
+
+
+def generate_definition_sheet(session: Session, form: int):
+    definitions = crud_question.get_definition(session=session, form=form.id)
+    df = pd.DataFrame(definitions)
+    df["type"] = df["type"].apply(lambda x: str(x).split(".")[1])
+    return df.groupby(["id", "question", "type", "option"]).first()
+
+
 def generate_excel_template(session: Session, form: int):
     form = crud_form.get_form_by_id(session=session, id=form)
     questions = crud_question.get_excel_question(session=session, form=form.id)
-    definitions = crud_question.get_definition(session=session, form=form.id)
-    definitions = pd.DataFrame(definitions)
-    definitions["type"] = definitions["type"].apply(
-        lambda x: str(x).split(".")[1])
-    definitions = definitions.groupby(["id", "question", "type",
-                                       "option"]).first()
-    print(list(definitions))
     data = pd.DataFrame(columns=[q.to_excel_header for q in questions],
                         index=[0])
     form_name = humps.decamelize(form.name)
@@ -44,7 +68,20 @@ def generate_excel_template(session: Session, form: int):
     })
     for col_num, value in enumerate(data.columns.values):
         worksheet.write(0, col_num, value, header_format)
-    definitions.to_excel(writer, sheet_name='definitions', startrow=0)
+    definitions = generate_definition_sheet(session=session, form=form)
+    definitions.to_excel(writer,
+                         sheet_name='definitions',
+                         startrow=0,
+                         header=False)
+    administration = pd.read_csv("./source/administration-cascade.csv")
+    administration["administration"] = administration[
+        "parent"] + "|" + administration["name"]
+    administration = administration[["administration"]]
+    administration.to_excel(writer,
+                            sheet_name='administration',
+                            startrow=0,
+                            header=False,
+                            index=False)
     writer.save()
     return filepath
 
