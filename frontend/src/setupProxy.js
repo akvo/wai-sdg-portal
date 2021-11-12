21b27bba8435cf2bc162ba9004a3b9d37008ed5a
dedenbangkit <mail@dedenbangkit.com>
2021-11-13 01:05:47 +0700

    [#85] Modify Network and setupProxy
    - config.js pointed to localhost:5000/config.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -12,6 +12,16 @@ module.exports = function (app) {
     })
   );
   app.use(
+    ["/config.js"],
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "/",
+      },
+    })
+  );
+  app.use(
     ["/worker"],
     createProxyMiddleware({
       target: "http://localhost:5001",
