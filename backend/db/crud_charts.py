532a206a59dabb418e4a0ae0e4000774dbb08e8a
wayangalihpratama <wgprtm@gmail.com>
2021-11-16 17:09:01 +0800

    [#84] BE implement filter to chart endpoint

--- backend/db/crud_charts.py
+++ backend/db/crud_charts.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session, aliased
 from sqlalchemy import and_, func
 from models.answer import Answer
+from models.data import Data
+from models.views.view_data import ViewData
 import collections
 from itertools import groupby
 from typing import List
@@ -12,12 +14,25 @@ def get_chart_data(session: Session,
                    stack: int = None,
                    administration: List[str] = None,
                    options: List[str] = None):
+    # get list of data ids
+    data = session.query(Data.id).filter(Data.form == form)
+    if options:
+        data_id = session.query(ViewData.data).filter(
+            ViewData.options.contains(options)).all()
+        data = data.filter(Data.id.in_([d.data for d in data_id]))
+    if administration:
+        data = data.filter(Data.administration.in_(administration))
+    data = [d.id for d in data.all()]
+
+    # chart query
     type = "BAR"
     if stack:
         type = "BARSTACK"
         answerStack = aliased(Answer)
         answer = session.query(Answer.options, answerStack.options,
                                func.count())
+        # filter
+        answer = answer.filter(Answer.data.in_(data))
         answer = answer.join((answerStack,
                               Answer.data == answerStack.data))
         answer = answer.filter(and_(Answer.question == question,
@@ -43,6 +58,8 @@ def get_chart_data(session: Session,
     else:
         answer = session.query(Answer.options,
                                func.count(Answer.id))
+        # filter
+        answer = answer.filter(Answer.data.in_(data))
         answer = answer.filter(Answer.question == question)
         answer = answer.group_by(Answer.options)
         answer = answer.all()
