80c04a1f43a9b09a09cb0860ab6d9a46259f8e3a
dedenbangkit <mail@dedenbangkit.com>
2021-11-17 02:55:37 +0700

    [#84][#93] Final Seeder, Uganda Instance works 🔥 🔥 🔥

--- backend/source/geoconfig.py
+++ backend/source/geoconfig.py
@@ -22,16 +22,12 @@ class GeoLevels(enum.Enum):
     }]
     wai_uganda = [{
         "level": 0,
-        "name": "ADM1_EN",
-        "alias": "Adm 01"
+        "name": "ADM2_EN",
+        "alias": "District"
     }, {
         "level": 1,
-        "name": "ADM2_EN",
-        "alias": "Adm 02"
-    }, {
-        "level": 2,
         "name": "ADM4_EN",
-        "alias": "Adm 03"
+        "alias": "Sub-County"
     }]
     wai_bangladesh = [{
         "level": 0,
@@ -51,5 +47,5 @@ class GeoLevels(enum.Enum):
 class GeoCenter(enum.Enum):
     notset = [106.3715, -8.849025]
     wai_ethiopia = [38.6682, 7.3942]
-    wai_uganda = [33.0129, 2.5857]
+    wai_uganda = [33.3486, 2.9251]
     wai_bangladesh = [88.9238, 22.0201]
