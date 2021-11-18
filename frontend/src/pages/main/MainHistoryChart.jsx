211fac2c9552c485cab4ff3d1ccd7c8d55f10390
wayangalihpratama <wgprtm@gmail.com>
2021-11-18 11:54:03 +0800

    [#101] Load chart title & subtitle on history line chart

--- frontend/src/pages/main/MainHistoryChart.jsx
+++ frontend/src/pages/main/MainHistoryChart.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Row, Col, Card } from "antd";
+import { CloseSquareOutlined } from "@ant-design/icons";
 
 import "./main.scss";
 import { UIState } from "../../state/ui";
 import api from "../../util/api";
 import Chart from "../../chart";
 import isEmpty from "lodash/isEmpty";
+import upperFirst from "lodash/upperFirst";
 
 const MainHistoryChart = ({ current, data }) => {
   const { historyChart } = UIState.useState((s) => s);
   const [historyChartData, setHistoryChartData] = useState([]);
   const [selectedData, setSelectedData] = useState({});
 
-  console.log(data);
-
   useEffect(() => {
-    if (!isEmpty(historyChart)) {
+    if (!isEmpty(historyChart) && !isEmpty(data)) {
       const { dataPointId, question } = historyChart;
       const url = `history/${dataPointId}/${question?.id}`;
       api
@@ -39,17 +39,38 @@ const MainHistoryChart = ({ current, data }) => {
     } else {
       setHistoryChartData([]);
     }
-  }, [historyChart]);
+  }, [historyChart, data]);
 
   return (
     <Row align="middle" className="collapse-wrapper">
       <Col span={24} className="container">
         <Card
           className="visual-card-wrapper"
-          title="History Chart"
+          title={
+            <Row align="middle">
+              <Col align="start" span={12}>
+                History Chart
+              </Col>
+              <Col align="end" span={12}>
+                <CloseSquareOutlined
+                  onClick={() => {
+                    UIState.update((s) => {
+                      s.historyChart = {};
+                    });
+                  }}
+                />
+              </Col>
+            </Row>
+          }
           key="history-chart-card"
         >
-          <Chart type="LINE" data={historyChartData} wrapper={false} />
+          <Chart
+            title={`${selectedData?.name?.props?.name} Datapoint`}
+            subTitle={upperFirst(historyChart?.question?.name)}
+            type="LINE"
+            data={historyChartData}
+            wrapper={false}
+          />
         </Card>
       </Col>
     </Row>
