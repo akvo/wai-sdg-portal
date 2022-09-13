66a503f1db92f22c1ef65d9e34327f3b410b9b5d
dedenbangkit <mail@dedenbangkit.com>
2022-09-13 18:15:28 +0700

    [#196] Fix AnswerDictWithHistory model

--- backend/models/answer.py
+++ backend/models/answer.py
@@ -22,7 +22,7 @@ class AnswerDict(TypedDict):
 class AnswerDictWithHistory(TypedDict):
     history: bool
     question: int
-    value: Union[str, float, int, bool, dict, List[str], List[int],
+    value: Union[int, str, float, bool, dict, List[str], List[int],
                  List[float], None]
 
 
