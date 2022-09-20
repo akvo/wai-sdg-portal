8c80840eea921200959d1e4e11e915cb3b47ea9d
dedenbangkit <mail@dedenbangkit.com>
2022-09-20 13:02:32 +0700

    [#200] Coveralls setup

--- backend/test.sh
+++ backend/test.sh
@@ -1,3 +1,19 @@
-pytest -vvv -rP
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "Running tests"
+COVERAGE_PROCESS_START=./.coveragerc \
+  coverage run --parallel-mode --concurrency=multiprocessing --rcfile=./.coveragerc \
+  /usr/local/bin/pytest -vvv -rP
+
+echo "Coverage"
+coverage combine --rcfile=./.coveragerc
+coverage report -m --rcfile=./.coveragerc
+
+if [[ -n "${COVERALLS_REPO_TOKEN:-}" ]] ; then
+  coveralls
+fi
+
 ./storage.sh clear
 flake8
