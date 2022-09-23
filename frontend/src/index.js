84308e603210191284d1ffdf5603ace229351372
dedenbangkit <mail@dedenbangkit.com>
2022-09-23 15:39:51 +0700

    [#205] Remove reactdom

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
