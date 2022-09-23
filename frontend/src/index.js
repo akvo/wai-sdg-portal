49f9806e6bb21c2aa25eceb3165c6738e52003f0
dedenbangkit <mail@dedenbangkit.com>
2022-09-23 14:41:20 +0700

    [#205] Reacr 18 Render migrate

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,21 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 import "antd/dist/antd.css";
 
-ReactDOM.render(
-  <Auth0Provider
-    domain="wai-ethiopia.eu.auth0.com"
-    clientId="OlqShNF3knpLpwX7iPLUHFTr9BlrrkHF"
-    redirectUri={window.location.origin}
-    cacheLocation="localstorage"
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById("root")
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+    <Auth0Provider domain="wai-ethiopia.eu.auth0.com" clientId="OlqShNF3knpLpwX7iPLUHFTr9BlrrkHF" redirectUri={window.location.origin} cacheLocation="localstorage">
+        <App />
+    </Auth0Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
