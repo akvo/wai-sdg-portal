ceced4122f727fe4992a9777fdd5ea7748e4da04
wayangalihpratama <wgprtm@gmail.com>
2022-09-23 18:51:09 +0800

    [#204] Fix webform editor test

--- backend/tests/test_17_webform_editor.py
+++ backend/tests/test_17_webform_editor.py
@@ -1,5 +1,6 @@
 import sys
 import pytest
+import random
 from fastapi import FastAPI
 from httpx import AsyncClient
 from tests.test_01_auth import Acc
@@ -11,18 +12,18 @@ sys.path.append("..")
 account = Acc(True)
 
 form_definition = {
-    "id": 903430001,
+    "id": random.randint(1, 10000),
     "name": "New Form",
     "description": "New Form Description",
     "question_group": [{
-        "id": 903430556,
+        "id": random.randint(1, 10000),
         "name": "Consequat porta lorem",
         "order": 1,
         "repeatable": False,
         "question": [{
-            "id": 903430557,
+            "id": random.randint(1, 10000),
             "order": 1,
-            "questionGroupId": 903430556,
+            "questionGroupId": random.randint(1, 10000),
             "name": "Donec amet tincidunt dapibus ipsum aliquet",
             "type": "option",
             "required": False,
@@ -30,12 +31,12 @@ form_definition = {
                 "code": None,
                 "name": "New Option 1",
                 "order": 1,
-                "id": 904875904
+                "id": random.randint(1, 10000)
             }, {
                 "code": None,
                 "name": "New Option 2",
                 "order": 2,
-                "id": 904875905
+                "id": random.randint(1, 10000)
             }]
         }]
     }]
