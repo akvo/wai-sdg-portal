aa73d5f3e1654c3a7d27ffdbdb9ff0c4bdd2f2ea
wayangalihpratama <wgprtm@gmail.com>
2022-09-28 09:01:26 +0800

    [#205] Mock axios & ./util/api

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,13 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import TestApp from './TestApp';
 import '@testing-library/jest-dom';
+import { act } from 'react-dom/test-utils';
 
 jest.mock('leaflet');
-// jest.mock('axios');
+jest.mock('axios');
+jest.mock('./util/api', () => {
+  return {
+    get: () => new Promise((resolve) => resolve({ data: { data: [] } })),
+    setToken: () => {},
+  };
+});
 
 describe('App', () => {
-  test('test if the login button exists', () => {
-    render(<TestApp />);
+  test('test if the login button exists', async () => {
+    await act(async () => render(<TestApp />));
     const linkElement = screen.getByText(/Login or Signup/i);
     expect(linkElement).toBeInTheDocument();
   });
