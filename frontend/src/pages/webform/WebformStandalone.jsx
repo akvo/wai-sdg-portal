3bce23f1945d19e1e187869c3913cd497e91b7f0
wayangalihpratama <wgprtm@gmail.com>
2023-01-06 16:34:44 +0800

    [#228] Handle submission for standalone webform

--- frontend/src/pages/webform/WebformStandalone.jsx
+++ frontend/src/pages/webform/WebformStandalone.jsx
@@ -4,13 +4,51 @@ import { Webform } from 'akvo-react-form';
 import 'akvo-react-form/dist/index.css';
 import { UIState } from '../../state/ui';
 import WebformLogin from './WebformLogin';
-import { Row, Col } from 'antd';
+import { Row, Col, notification } from 'antd';
+import api from '../../util/api';
+
+const { notificationText } = window.i18n;
 
 const WebformStandalone = ({ match }) => {
   const uuid = match?.params?.uuid;
-  const { isLogin, formValue /*submitter*/ } = UIState.useState(
+  const { isLogin, formValue, submitter } = UIState.useState(
     (s) => s.webformLogin
   );
+
+  const onFinish = (values) => {
+    let data = Object.keys(values).map((v) => {
+      // do not transfrom datapoint to post params
+      if (values[v] && v !== 'datapoint') {
+        return { question: parseInt(v), value: values[v] };
+      }
+      return false;
+    });
+    data = data.filter((x) => x);
+    api
+      .post(
+        `data/form-standalone/${formValue?.id}?submitter=${submitter}`,
+        data
+      )
+      .then((res) => {
+        notification.success({
+          message: `${res.data.id} - ${res.data.name} submitted.`,
+        });
+        setTimeout(() => {
+          UIState.update((s) => {
+            s.webformLogin = {
+              submitter: null,
+              isLogin: false,
+              formValue: {},
+            };
+          });
+        }, 1000);
+      })
+      .catch(() => {
+        notification.error({
+          message: notificationText?.errorText,
+        });
+      });
+  };
 
   if (!isLogin) {
     return <WebformLogin uuid={uuid} />;
@@ -25,8 +63,7 @@ const WebformStandalone = ({ match }) => {
         >
           <Webform
             forms={formValue}
-            // onFinish={onFinish}
-            // onChange={onChange}
+            onFinish={onFinish}
             sticky={true}
           />
         </Col>
