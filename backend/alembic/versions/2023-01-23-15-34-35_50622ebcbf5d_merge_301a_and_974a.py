7af002b1701f7c18c8d6a35a6a81709ca8047fd1
ifirmawan <firmawaneiwan@gmail.com>
2023-01-23 15:58:01 +0700

    [#266] Fix multiple heads alembic migration

---
+++ backend/alembic/versions/2023-01-23-15-34-35_50622ebcbf5d_merge_301a_and_974a.py
@@ -0,0 +1,21 @@
+"""merge 301a and 974a
+
+Revision ID: 50622ebcbf5d
+Revises: 301adfe1cccc, 974a3418ace8
+Create Date: 2023-01-23 15:34:35.063600
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '50622ebcbf5d'
+down_revision = ('301adfe1cccc', '974a3418ace8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
