5d04a3bd91f3a5d0db89e469a4ea47ae04dab195
dedenbangkit <mail@dedenbangkit.com>
2023-01-31 14:27:08 +0700

    [#270] Add documentation build to dc.sh

--- dc.sh
+++ dc.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ $1 == "up" ]]; then
+rm -rf docs/"${INSTANCE_NAME}"/build/html/*
+docker run -it --rm -v "$(pwd)/docs/${INSTANCE_NAME}:/docs" \
+    akvo/akvo-sphinx:20220829.093307.b5bbf28 make html
+fi
+
 COMPOSE_HTTP_TIMEOUT=180 docker compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose-docker-sync.yml "$@"
