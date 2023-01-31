102c92d26ce4bc367d05e1ee946051f93c6d99f6
ifirmawan <firmawaneiwan@gmail.com>
2023-01-31 14:28:34 +0700

    [#254] Remove optional param database in bash run
    On jmp-explorer.akvotest.org there is no need to use the optional
    database param because the database name is the same as the instance
    name.

--- backend/run.sh
+++ backend/run.sh
@@ -2,7 +2,7 @@
 
 if [[ -z "${SKIP_MIGRATION}" ]]; then
     alembic upgrade head
-    akvo-responsegrouper --config $(echo "./source/${INSTANCE_NAME}/category.json") --database $(echo $DATABASE_URL | sed 's/-/_/g')
+    akvo-responsegrouper --config $(echo "./source/${INSTANCE_NAME}/category.json")
 fi
 
 python main.py
