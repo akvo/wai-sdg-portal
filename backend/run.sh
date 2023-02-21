f15d61af301147e54c2b622f1ed43d96018ecbc4
ifirmawan <firmawaneiwan@gmail.com>
2023-02-21 14:52:11 +0700

    [#286] Add category.json to notset and healthcheck

--- backend/run.sh
+++ backend/run.sh
@@ -7,4 +7,9 @@ if [ "${SANDBOX_STATUS}" = "true" ]; then
 	echo "${SANDBOX_DATA_SOURCE}"
 fi
 
+if [[ -z "${SKIP_MIGRATION}" ]]; then
+    alembic upgrade head
+    akvo-responsegrouper --config $(echo "./source/${INSTANCE_NAME}/category.json") --database $(echo $DATABASE_URL | sed 's/-/_/g')
+fi
+
 python main.py
