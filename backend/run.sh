71db1212cf26e1949f48e20a58802b5e10a217d8
dedenbangkit <mail@dedenbangkit.com>
2023-02-22 09:14:03 +0700

    [#287] Sandbox Environment Tryout

--- backend/run.sh
+++ backend/run.sh
@@ -4,4 +4,9 @@ if [[ -z "${SKIP_MIGRATION}" ]]; then
     alembic upgrade head
 fi
 
+if [[ "${SANDBOX_STATUS}" = "true" ]]; then
+	echo "This is sandbox"
+	echo "${SANDBOX_DATA_SOURCE}"
+fi
+
 python main.py
