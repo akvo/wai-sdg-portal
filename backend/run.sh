d08373a3599fd839e878ada0ad8fc11496a9d835
dedenbangkit <mail@dedenbangkit.com>
2023-02-23 16:03:19 +0700

    [#286] Modify runner

--- backend/run.sh
+++ backend/run.sh
@@ -16,9 +16,4 @@ if [ -f "${CATEGORIES}" ]; then
 	echo "done"
 fi
 
-if [[ -z "${SKIP_MIGRATION}" ]]; then
-    alembic upgrade head
-    akvo-responsegrouper --config $(echo "./source/${INSTANCE_NAME}/category.json") --database $(echo $DATABASE_URL | sed 's/-/_/g')
-fi
-
 python main.py
