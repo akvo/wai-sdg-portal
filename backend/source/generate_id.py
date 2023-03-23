343c4654d2fbc01c7c03a1b21b98e5b1d033c8da
wayangalihpratama <wgprtm@gmail.com>
2023-03-23 20:44:50 +0800

    [#312] Refactor id generator, generate qid if not defined

--- backend/source/generate_id.py
+++ backend/source/generate_id.py
@@ -1,5 +1,6 @@
 import json
 import os
+import numpy as np
 import pandas as pd
 
 source_path = os.environ["INSTANCE_NAME"]
@@ -13,19 +14,36 @@ files = list(filter(lambda x: ".bak" not in x, os.listdir(file_path)))
 for file in sorted(files):
     with open(f'{file_path}{file}') as json_file:
         data = json.load(json_file)
-    form_id = int(data['id'])
-    question_groups = data['question_groups']
-    df = pd.DataFrame(columns=['question_group_id', 'question_group'])
-    for i, group in enumerate(question_groups):
-        group_name = group['question_group']
-        group_id = form_id + i + 1
-        df = pd.concat([
-            df,
-            pd.DataFrame({
-                'question_group_id': group_id,
-                'question_group': group_name
-            }, index=[i])])
-    for i, group in enumerate(question_groups):
-        group['id'] = df.loc[i, 'question_group_id']
+    df = pd.json_normalize(
+        data,
+        record_path=['question_groups', 'questions'],
+        meta=[
+            'form', 'id',
+            ['question_groups', 'question_group']],
+        record_prefix='question_')
+    # generate unique IDs for question groups and questions
+    qg_length = len(data.get("question_groups"))
+    df = df.replace(np.nan, None)
+    df['qgid'] = df.apply(lambda row: int(row['id'] + row.name + 1), axis=1)
+    df['qid'] = df.apply(
+        lambda row: int(row['qgid'] + row.name + 1 + qg_length)
+        if not row["question_id"] else int(row["question_id"]),
+        axis=1)
+    # add question group and question id to json
+    for i, qg in enumerate(data.get("question_groups")):
+        find_qg = df.loc[
+            df["question_groups.question_group"] == qg.get("question_group")
+        ].head(1)
+        qg["id"] = int(find_qg["qgid"].values[0])
+
+        for i, q in enumerate(qg.get("questions")):
+            if "id" in q:
+                continue
+            find_q = df.loc[
+                df["question_question"] == q.get("question")
+            ].head(1)
+            q["id"] = int(find_q["qid"].values[0])
+    # write json
     with open(f'{file_path}{file}', 'w') as json_file:
-        json.dump(data, json_file)
+        json.dump(data, json_file, indent=2)
+    print(f"Transform Form: {data.get('form')}")
