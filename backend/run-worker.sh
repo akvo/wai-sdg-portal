b8b3e8dc5639a3b45fb689038669c0eabe7e1c53
dedenbangkit <mail@dedenbangkit.com>
2023-04-28 16:30:33 +0700

    [#334] Remove category init check for worker

--- backend/run-worker.sh
+++ backend/run-worker.sh
@@ -2,18 +2,4 @@
 
 set -eu
 
-CATEGORIES="./source/${INSTANCE_NAME}/category.json"
-
-if [ "${SANDBOX_STATUS}" = "true" ]; then
-	echo "This is sandbox"
-	echo "${SANDBOX_DATA_SOURCE}"
-	CATEGORIES="./source/${SANDBOX_DATA_SOURCE}/category.json"
-fi
-
-if [ -f "${CATEGORIES}" ]; then
-  echo "${CATEGORIES} exists"
-	akvo-responsegrouper -c ${CATEGORIES}
-	echo "done"
-fi
-
 python worker.py
