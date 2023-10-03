c1d83c928c27383d93151d3c59f78464c1eeb53e
dedenbangkit <mail@dedenbangkit.com>
2023-10-03 20:47:21 +0700

    [#420] Modify run-worker.sh

--- backend/run-worker.sh
+++ backend/run-worker.sh
@@ -2,4 +2,15 @@
 
 set -eu
 
+CATEGORIES="./source/${INSTANCE_NAME}/category.json"
+
+if [ "${SANDBOX_STATUS}" = "true" ]; then
+	echo "This is sandbox"
+	echo "${SANDBOX_DATA_SOURCE}"
+	CATEGORIES="./source/${SANDBOX_DATA_SOURCE}/category.json"
+fi
+
+# Copy the content of $CATEGORIES to "./.category.json"
+echo "$(<"$CATEGORIES")" > "./.category.json"
+
 python worker.py
