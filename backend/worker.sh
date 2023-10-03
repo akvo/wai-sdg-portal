83569828099fd314f85ca52291acbfd8691798c2
dedenbangkit <mail@dedenbangkit.com>
2023-10-03 20:23:56 +0700

    [#420] Manual copy category json in the worker

--- backend/worker.sh
+++ backend/worker.sh
@@ -6,4 +6,15 @@ pip -q install --upgrade pip
 pip -q install --cache-dir=.pip -r requirements.txt
 pip check
 
+CATEGORIES="./source/${INSTANCE_NAME}/category.json"
+
+if [ "${SANDBOX_STATUS}" = "true" ]; then
+	echo "This is sandbox"
+	echo "${SANDBOX_DATA_SOURCE}"
+	CATEGORIES="./source/${SANDBOX_DATA_SOURCE}/category.json"
+fi
+
+# Copy the content of $CATEGORIES to "./category.json"
+echo "$(<"$CATEGORIES")" > "./category.json"
+
 uvicorn worker:worker --reload --port 5001
