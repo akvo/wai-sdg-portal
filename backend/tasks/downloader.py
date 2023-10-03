55712be97dccb02356ad33aa150df51bd1705dd4
wayangalihpratama <wgprtm@gmail.com>
2023-10-03 16:16:30 +0800

    [#420] Finalize & add test for include jmp service on exported file

--- backend/tasks/downloader.py
+++ backend/tasks/downloader.py
@@ -8,18 +8,21 @@ from sqlalchemy.orm import Session
 from util.helper import HText
 
 
+DEFAULT_COL_NAMES = col_names = [
+    "id",
+    "created_at",
+    "created_by",
+    "updated_at",
+    "updated_by",
+    "datapoint_name",
+    "administration",
+    "geolocation",
+]
+
+
 def rearange_columns(col_names: list, jmp_category_columns: list = []):
     col_question = list(filter(lambda x: HText(x).hasnum, col_names))
-    col_names = [
-        "id",
-        "created_at",
-        "created_by",
-        "updated_at",
-        "updated_by",
-        "datapoint_name",
-        "administration",
-        "geolocation",
-    ] + jmp_category_columns + col_question
+    col_names = DEFAULT_COL_NAMES + jmp_category_columns + col_question
     return col_names
 
 
@@ -46,19 +49,29 @@ def download(session: Session, jobs: dict, file: str):
         options=info["options"],
     )
     # add JMP values to data
-    for jc in jmp_category_names:
-        data = crud_jmp.get_jmp_overview(
-            session=session, form=info["form_id"], data=data, name=jc.lower())
-        for d in data:
-            if len(d["categories"]):
-                d[jc] = d["categories"][0]["category"]
-            else:
-                d[jc] = ""
-            d.pop("categories")
+    if data:
+        for jc in jmp_category_names:
+            data = crud_jmp.get_jmp_overview(
+                session=session, form=info["form_id"],
+                data=data, name=jc.lower())
+            for d in data:
+                if len(d["categories"]):
+                    d[jc] = d["categories"][0]["category"]
+                else:
+                    d[jc] = ""
+                d.pop("categories")
     # transform to pandas df
     df = pd.DataFrame(data)
     questions = crud_question.get_excel_headers(
         session=session, form=info["form_id"])
+    # adding columns if no data defined
+    if not data:
+        for col in DEFAULT_COL_NAMES:
+            if col not in list(df):
+                df[col] = ""
+        for jc in jmp_category_names:
+            if jc not in list(df):
+                df[jc] = ""
     for q in questions:
         if q not in list(df):
             df[q] = ""
@@ -73,8 +86,11 @@ def download(session: Session, jobs: dict, file: str):
         {"context": "Administration", "value": administration_name},
     ]
     for inf in info["tags"]:
-        context.append({"context": "Filters", "value": inf["q"] + ": " + inf["o"]})
-    context = pd.DataFrame(context).groupby(["context", "value"], sort=False).first()
+        context.append({
+            "context": "Filters", "value": inf["q"] + ": " + inf["o"]
+        })
+    context = pd.DataFrame(context).groupby(
+        ["context", "value"], sort=False).first()
     context.to_excel(writer, sheet_name="context", startrow=0, header=False)
     workbook = writer.book
     worksheet = writer.sheets["context"]
